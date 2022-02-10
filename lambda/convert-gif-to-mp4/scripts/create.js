94b41f7f835e5824a7c625a94cf107a01c532468
John Carter <johncarter7061@gmail.com>
2022-02-10 09:29:48 -0500

    User IAM role

--- lambda/convert-gif-to-mp4/scripts/create.js
+++ lambda/convert-gif-to-mp4/scripts/create.js
@@ -1,20 +1,19 @@
-const { execSync } = require('child_process');
+const { execSync } = require("child_process");
 
 const metadata = {
-  'function-name': 'convertToMP4FromGIF',
-  'zip-file': 'fileb://dist/function.zip',
-  'runtime': 'nodejs10.x',
-  'handler': 'index.handler',
-  'role': 'arn:aws:iam::484931367994:role/S3-Lambda-Role'
+  "function-name": "convertToMP4FromGIF",
+  "zip-file": "fileb://dist/function.zip",
+  runtime: "nodejs10.x",
+  handler: "index.handler",
+  role: "arn:aws:iam::484931367994:role/S3-Lambda-Role", // IAM Role
 };
 
-const options = Object.entries(metadata).reduce(
-  (query, [key, value]) => {
+const options = Object.entries(metadata)
+  .reduce((query, [key, value]) => {
     query.push(`--${key}`);
     query.push(value);
     return query;
-  },
-  []
-).join(' ');
+  }, [])
+  .join(" ");
 
 execSync(`aws lambda create-function ${options}`);
